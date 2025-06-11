016b4be9bfb08f8796edc60d77aafd2570bb27c2
rabbitmaid <rabbitmaid@proton.me>
2025-06-11 16:59:41 +0100

    Remove unwanted files and correct tailwindcss v4 setup

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,7 +33,7 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
-        Auth::login($user);
+        // Auth::login($user);
 
         return redirect('/login')->with('status', 'Registration successful. Please log in.');
     }
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,36 +1,3 @@
 import './bootstrap';
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from 'tailwindcss';
-
-export default defineConfig({
-  plugins: [
-    laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
-      refresh: true,
-    }),
-  ],
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss(),
-      ],
-    },
-  },
-});
 import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-module.exports = {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
 Alpine.start();
